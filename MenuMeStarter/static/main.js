35a20996de76a382c6bca8be5dd975a3ff7c34c8
redbolder <37903573+iliaamiri@users.noreply.github.com>
2021-10-12 21:11:31 -0700

    commenting

---
+++ MenuMeStarter/static/main.js
@@ -0,0 +1,5 @@
+/*
+* Added by Ilia
+*
+* Any general javascript code goes here.
+* */
--- MenuMeStarter/static/menus.js
+++ MenuMeStarter/static/menus.js
@@ -1,4 +1,9 @@
+/*
+    Added by Ilia
+* This file is for handling AJAX request and response for presenting menus. For the sake of the server's security,
+* I found it to be necessary to have an extra node.js module that could handle this process.
+* */
+
 if (!window.location.hash){
     window.location.href = "./404"
 }
-
